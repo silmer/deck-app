e960297bb43fdeba6147be3a31b17b11a2027658
siljemda <dahl.silmer@gmail.com>
2017-12-25 21:24:39 +0100

    Demo

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import pg from 'pg';
+import express from 'express';
+
+var app = express();
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
